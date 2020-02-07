35f962977b2c63a8eea2fc68c472bd701cb516e5
Nadia <atiyeh.nadiaa@gmail.com>
2020-02-07 12:57:06 -0700

    adde

--- lab3Module/src/GOLTester.java
+++ lab3Module/src/GOLTester.java
@@ -1,5 +1,7 @@
 package lab3;
 
 public class GOLTester {
+    public static void main(String [] args){
 
+    }
 }
--- lab3Module/src/GameOfLife.java
+++ lab3Module/src/GameOfLife.java
@@ -5,6 +5,23 @@ public class GameOfLife {
     int [][] board;
     int [][] previous;
 
+    public static void main (String [] args){
+        System.out.print("Hello");
+//        int [][] testArray = {{0,0,0,0,0},
+//                {0,0,0,0,0},
+//                {0,1,1,1,0},
+//                {0,0,0,0,0},
+//                {0,0,0,0,0}};
+//        GameOfLife test1 = new GameOfLife(testArray);
+//        test1.getBoard();
+//        test1.neighbors(3,3);
+//        test1.evolution();
+//        test1.oneStep();
+//        test1.convertArray(testArray);
+//
+
+    }
+
     public GameOfLife (){
     }
 
@@ -20,7 +37,7 @@ public class GameOfLife {
         previous = new int[size][size];
         for(int i = 0; i < a.length; i++){
             for(int j = 0; j < a[i].length;j++){
-                previous = a[i][j];
+                previous[i][j] = a[i][j];
             }
         }
     }
@@ -29,39 +46,52 @@ public class GameOfLife {
         return board;
     }
 
-    public void oneStep(){
-        board = evolution()
+//    public void convertArray(int[][] a){
+//        for(int i = 0; i < a.length; i++){
+//            for(int j = 0; j < a.length; j++){
+//                if(a[i][j] == 0){
+//                    System.out.println("*");
+//                } else{
+//                    System.out.println("!");
+//                }
+//            }
+//        }
+//    }
+
+    public int[][] oneStep(){
+        board = evolution();
+        return board;
     }
 
     public int neighbors(int row, int col){
-        int neighbors = 0;
+        int aliveNeighbors = 0;
         int dead = 0;
         int alive = 1;
-        for(int i = row - 1; i < row + 1; i++){
-            for(int j = col - 1; j < col +1; j++){
+        for(int i = row + 1; i <= row - 1; i++){
+            for(int j = col + 1; j <= col - 1; j++){
                 if(board[i][j] == alive && (row != i|| col != j)){
-                    neighbors ++;
+                    aliveNeighbors ++;
                 }
             }
         }
-        return neighbors;
+        return aliveNeighbors;
     }
 
-    public int [][] evolution(int n){
+    public int [][] evolution(){
         int dead = 0;
-        int alive = 1;
-        for(int i = 1; i < n; i++){
-            for(int j = 1; j< n; j++){
+        int alive = 0;
+        for(int i = 0; i < size -1; i++){
+            for(int j = 0; j < size -1; j++){
                 int neighbors = neighbors(i,j);
                 if(board[i][j] == alive){
-                    if((neighbors < 2)) || (neighbors > 3)){
+                    if((neighbors < 2) || (neighbors > 3)){
                         previous[i][j]= dead;
                     }
-                    if((neighbors == 2) || (neighbors == 3)){
+                    else if((neighbors == 2) || (neighbors == 3)){
                         previous[i][j] = alive;
                     }
                 }
-                if(board[i][j] == dead & neighbors == 3){
+                else if(board[i][j] == dead & neighbors == 3){
                     previous[i][j] = alive;
                 }
             }
@@ -70,3 +100,5 @@ public class GameOfLife {
     }
 
 }
+
+
